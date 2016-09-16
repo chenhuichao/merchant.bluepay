b1547e78b6c34630df9680d9e0d05d4e8e9dde87
chenhuichao <mliuweidong@hotmail.com>
2016-09-17 03:50:31 +0800

    .

--- src/application/controller/admin/merchant/pos/index.php
+++ src/application/controller/admin/merchant/pos/index.php
@@ -21,16 +21,15 @@ $data = array(
 echo json_encode($data);
 
 function getData($results){
-	$html = '<table class="table table-striped table-bordered">
-			<tbody role="alert" aria-live="polite" aria-relevant="all">';
+	$html = '<table class="table table-striped table-bordered"><tbody role="alert" aria-live="polite" aria-relevant="all">';
 	foreach($results as $row){
 		$record = UserSvc::getById($row['user_id']);
-		$html .= '<tr><th>'.$_LANG_['framework.public.id'].'</th><td>'.$row['id'].'</td></tr>';
+		$html .= '<tr><th>FFFFUK'.$_LANG_['framework.public.id'].'</th><td>'.$row['id'].'</td></tr>';
 		$html .= '<tr><th>'.$_LANG_['framework.public.sn'].'</th><td>'.$row['sn'].'</td></tr>';
 		$html .= '<tr><th>'.$_LANG_['framework.public.merchant_id'].'</th><td>'.$row['merchant_id'].'</td></tr>';
-		$html .= '<tr><th>'.$_LANG_['framework.public.bind_user_id'].'</th><td>'.$result['user_id'].'</td></tr>';
+		$html .= '<tr><th>'.$_LANG_['framework.public.bind_user_id'].'</th><td>'.$row['user_id'].'</td></tr>';
 		$html .= '<tr><th>'.$_LANG_['framework.public.mobile'].'</th><td>'.$record->mobile.'</td></tr>';
-		$html .= '<tr><th>'.$_LANG_['framework.public.ctime'].'</th><td>'.$result['ctime'].'</td></tr>';
+		$html .= '<tr><th>'.$_LANG_['framework.public.ctime'].'</th><td>'.$row['ctime'].'</td></tr>';
 	}
     $html .= '</tbody></table>';
 
