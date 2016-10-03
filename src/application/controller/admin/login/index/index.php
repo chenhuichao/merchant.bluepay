879ed48a4b8b2f5b040e1ecd4752690d776d3e91
chenhuichao <mliuweidong@hotmail.com>
2016-10-03 23:27:58 +0800

    *

--- src/application/controller/admin/login/index/index.php
+++ src/application/controller/admin/login/index/index.php
@@ -10,5 +10,8 @@ if($uid && LoaderSvc::loadSess()->get('APP_NAME') == APP_NAME){
 	if(isset($_GET['lang']) && in_array($_GET['lang'],['zh-CN','en-US'])){
 		setcookie("_lang",$_GET['lang'],time() + 2592000);
 		header("location:/login");
+	}else{
+		LoaderSvc::loadSess()->set('returl',urlencode($returl));
+		LoaderSvc::loadSmarty()->display('login/index.tpl');
 	}
 }
